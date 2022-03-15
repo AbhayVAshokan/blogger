b63a0a2aa7d0b6dc84a0172ff85ee33260a69c81
AbhayVAshokan <abhayvashokan@gmail.com>
2022-03-15 21:01:19 +0530

    Added react-query package

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import { Routes, Route, Navigate } from "react-router-dom";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 import Dashboard from "./components";
 import Edit from "./components/Edit";
@@ -13,23 +15,31 @@ const App = () => {
   // Dummy state to simulate authentication.
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const queryClient = new QueryClient();
+
   return (
-    <main className="container">
-      <NavBar handleLogout={() => setIsLoggedIn(false)} />
-      <Routes>
-        <Route path="login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
-        {isLoggedIn ? (
-          <>
-            <Route path=":userId/posts" element={<Dashboard />} />
-            <Route path=":userId/posts/new" element={<New />} />
-            <Route path=":userId/posts/:postId/edit" element={<Edit />} />
-            <Route path=":userId/posts/:postId" element={<Post />} />
-          </>
-        ) : (
-          <Route path="*" element={<Navigate to="login" />} />
-        )}
-      </Routes>
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main className="container">
+        <NavBar handleLogout={() => setIsLoggedIn(false)} />
+        <Routes>
+          <Route
+            path="login"
+            element={<Login setIsLoggedIn={setIsLoggedIn} />}
+          />
+          {isLoggedIn ? (
+            <>
+              <Route path=":userId/posts" element={<Dashboard />} />
+              <Route path=":userId/posts/new" element={<New />} />
+              <Route path=":userId/posts/:postId/edit" element={<Edit />} />
+              <Route path=":userId/posts/:postId" element={<Post />} />
+            </>
+          ) : (
+            <Route path="*" element={<Navigate to="login" />} />
+          )}
+        </Routes>
+      </main>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 };
 
